40235a2984c5d0fa83a5c22576127b5430c30c29
cris-paz14 <60517048+cris-paz14@users.noreply.github.com>
2020-02-05 13:03:02 -0600

    shhhh

--- controladores/prueba.php
+++ controladores/prueba.php
@@ -1,10 +1,9 @@
 <?php
-<<<<<<< HEAD
 echo"HOLA";
 function prueba(){
-    echo "primera funcion ";
+    echo "primera funcion";
+
 }
-=======
-echo"HOLA PERRAS";
->>>>>>> 9c6a3d97170687c7fa2ebde16ad6dda4f5422305
-?
+    echo "HOLA PERRA";
+
+?>
